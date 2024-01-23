fb7fb3dc83a9a97ab807fe01e25feee35b80c3d6
harshamalapati <harshamalapati444@gmail.com>
2024-01-23 20:18:23 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
-public class demo{
-public void main(String[] args){
-System.out.println("");
-}
+class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
